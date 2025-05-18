2cdff55c39b1e725b110a343e13b2d1fef6cbdac
jadedilin <xiaoyushan@protonmail.com>
2025-05-18 11:42:45 +0100

    Rescue from Record Not Found error with helper method root_path

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -5,5 +5,8 @@ class BlogPostsController < ApplicationController
 
   def show
     @blog_post = BlogPost.find(params[:id])
+  # Helper method to redirect to root ("/") when show action doesn't return a blog post
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
   end
 end
